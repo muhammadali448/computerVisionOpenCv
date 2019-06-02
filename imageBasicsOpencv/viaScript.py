e9f5c7449a190a498a960d29445d48e34070fd4a
muhammadali448 <muhammadali19960@outlook.com>
2019-06-02 17:32:22 +0500

    drawing opencv

---
+++ imageBasicsOpencv/viaScript.py
@@ -0,0 +1,9 @@
+import cv2
+img = cv2.imread("../DATA/00-puppy.jpg")
+newImg = cv2.resize(img, (960, 540))
+while True:
+    if cv2.waitKey(1) & 0xFF == 27:
+        break      
+    cv2.imshow("Puppy", newImg)
+
+cv2.destroyAllWindows()
