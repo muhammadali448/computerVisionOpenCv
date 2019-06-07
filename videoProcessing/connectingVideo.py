28ca13bbc0e444ba33bf4aa4265654a89c0910f3
muhammadali448 <muhammadali19960@outlook.com>
2019-06-08 02:45:26 +0500

    modified extension connecting video

--- videoProcessing/connectingVideo.py
+++ videoProcessing/connectingVideo.py
@@ -3,7 +3,7 @@ cap = cv2.VideoCapture(0)
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-writer = cv2.VideoWriter("../videos/video_1.mp4v", fourcc, 20.0, (width,height))
+writer = cv2.VideoWriter("../videos/video_1.mp4", fourcc, 20.0, (width,height))
 
 while True:
     ret, frame = cap.read()
